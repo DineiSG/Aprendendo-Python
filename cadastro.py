b87cf126c399fec4987a5c41e4dee64a9515391b
Dinei <waldinei.wsg@gmail.com>
2023-02-18 14:55:33 -0300

    Finalizado o codigo Cadastro

--- cadastro.py
+++ cadastro.py
@@ -5,20 +5,44 @@
 #versão: 0.0
 #...
 
+
 print ("Por favor informe os dados do cliente")
 nome=input("Nome:")
 email=input("Email:")
 senha=input("Senha:")
+confirmesenha=input("Confirme a senha: ")
+while confirmesenha != senha:
+    breakpoint
 endereco=input("Endereco:")
-estado=input("Estado:")
 cidade=input("Cidade:")
+estado=input("Estado:")
 cep=input("CEP:") 
+print("\n")
+
+print("---------------------------------------------------------------------")
 
 print("Favor confirmar os dados informados")
+print("\n")
 print("Nome: ", nome)
 print("Email: ", email)
 print("Senha: ", senha)
-print("Endereco: ", endereco, estado, cidade, cep)
+print("Endereco: ", endereco,"/""Cidade: ",cidade,"/""Estado: ",estado,"/""CEP: ",cep)
+print("\n")
+
+print("---------------------------------------------------------------------")
 
 
-   
+print("Os dados estão corretos ?")
+confirmação=input("")
+
+print("\n")
+
+print("---------------------------------------------------------------------")
+
+if confirmação =="sim" :
+    print("Cadastro realizado com sucesso. O login do cliente será o e-mail fornecido.  ")
+else:
+    print("Favor informar os dados corretamente")
+    
+
+       
