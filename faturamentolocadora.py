7057b0023982b195f6438ac20392a017e5025dba
Dinei <waldinei.wsg@gmail.com>
2023-03-07 00:13:45 -0300

    "Alterado o arquivo faturamentolocadora.py"

--- faturamentolocadora.py
+++ faturamentolocadora.py
@@ -10,14 +10,15 @@
 print("_______________________________Controle de Faturamento___________________________")
 print("\n")
 qntveiculos=int(input("Informe a quantidade de veículos: "))
+valorDiaria=float(input("Informe o valor da diária:R$ "))
 
 #Calculo de Ganho Anual
-valoraluguelmes=1500
+valoraluguelmes=valorDiaria*30
 quant_veic_loc=qntveiculos/3
 fat_anual=quant_veic_loc*valoraluguelmes*12
 
 #Calculo de Multas
-valormulta=valoraluguelmes*10/100+valoraluguelmes
+valormulta=valoraluguelmes*10/100
 devol_atrasadas=quant_veic_loc/10
 ganhomultas=valormulta*devol_atrasadas
 
@@ -31,5 +32,5 @@ print("\n")
 print("Quantidade de Locações por Mes: ",("%4.0f" %quant_veic_loc))
 print("Quantidade de Veículos em Estoque ao Fim do Ano: ",int(qnt_veic_final))
 print("Veículos Devolvidos com Atraso por Mes:",("%4.0f" %devol_atrasadas))
-print("Lucro com Multas por Atrasos Por Mês:R$",float(ganhomultas))
-print("Faturamento Anual:R$",float(fat_anual))
+print("Lucro com Multas por Atrasos Por Mês:R$",float("%4.2f"%ganhomultas))
+print("Faturamento Anual:R$",float("%9.2f"%fat_anual))
